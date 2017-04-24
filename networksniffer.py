eabca5f7ff8aa7f9bb7904d9e50a6d63af213641
Hydroxyl <idnarosirrah@gmail.com>
2017-04-23 20:26:14 -0500

    Added scripts for network sniffer and port scanner

---
+++ networksniffer.py
@@ -0,0 +1,156 @@
+# Packet sniffer
+import socket
+import sys
+from struct import *
+
+# creating ethernet header
+def eth_address(a):
+    b = "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % (ord(a[0]), ord(a[1]), ord(a[2]), ord(a[3]), ord(a[4]), ord(a[5]))
+    return b
+
+# Creating a socket
+try:  
+    sock = socket.socket( socket.AF_PACKET , socket.SOCK_RAW , socket.ntohs(0x0003))
+except socket.error ,msg:
+    print 'Socket could not be created. Error code: ' + str(msg[0]) + 'Message' + msg[1]
+    sys.exit()
+
+
+# Receiving a packet
+while True:
+    packet = sock.recvfrom(65565)
+    # get packet data and place in tuple
+    packet = packet[0]
+
+    # parse ethernet header
+    eth_length = 14
+
+    eth_header = packet[:eth_length]
+    eth = unpack('!6s6sH', eth_header)
+    eth_proto = socket.ntohs(eth[2])
+    print 'Destination MAC : ' + eth_address(packet[0:6]) + '\n'
+    print 'Source MAC : ' + eth_address(packet[6:12]) + '\n'
+    print 'Protocol :  ' + str(eth_proto) + '\n'
+
+    # Parse IP packets, IP protocol number = 8
+    if eth_proto == 8:
+        # place first 20 characters into tuple,this is ip header
+        ip_header = packet[eth_length:20 + eth_length]
+        # unpack IP header
+        iph = unpack('!BBHHHBBH4s4s', ip_header)
+
+        version_ihl = iph[0]
+        version = version_ihl >> 4
+        ihl = version_ihl & 0xF
+
+        iph_length = ihl * 4
+        # Time to live
+        ttl = iph[5]
+        protocol = iph[6]
+        source_addr = socket.inet_ntoa(iph[8])
+        desti_addr = socket.inet_ntoa(iph[9])
+
+        print 'Version : ' + str(version) + '\n'
+        print 'IP Header length : ' + str(ihl) + '\n'
+        print 'TTL : ' + str(ttl) + '\n'
+        print 'Protocol : ' + str(protocol) + '\n'
+        print 'Source Address : ' + str(source_addr) + '\n'
+        print 'Destination Address :' + str(desti_addr) + '\n'
+
+        # TCP protocol
+        if protocol == 6:
+            tcp = iph_length + eth_length
+            # Set TCP Header variable
+            tcp_header = packet[tcp:tcp + 20]
+
+            # Unpack TCP header
+            tcph = unpack('!HHLLBBHHH', tcp_header)
+
+            source_port = tcph[0]
+            dest_port = tcph[1]
+            sequence = tcph[2]
+            acknowledgement = tcph[3]
+            doff_reserved = tcph[4]
+            tcph_length = doff_reserved >> 4
+            print 'Source Port : ' + str(source_port) + '\n'
+            print 'Destination Port : ' + str(dest_port) + '\n'
+            print 'Sequence Number : ' + str(sequence) + '\n'
+            print 'Acknowledgement : ' + str(acknowledgement) + '\n'
+            print 'TCP header length: ' + str(tcph_length) + '\n'
+
+            h_size = iph_length + tcph_length * 4
+            data_size = len(packet) - h_size
+
+            # extract data from packet
+
+            data = packet[h_size:]
+            print 'Data: ' + str(data)
+            print '\n'
+        # ICMP packets
+        elif protocol == 1:
+            u = iph_length + eth_length
+            icmph_length = 4
+            icmp_header = packet[u:u + 4]
+            # Now unpack
+            icmph = unpack('!BBH', icmp_header)
+            icmp_type = icmph[0]
+            code = icmph[1]
+            checksum = icmph[2]
+
+            print 'Type : ' + str(icmp_type) + '\n'
+            print 'Code : ' + str(code) + '\n'
+            print 'Checksum : ' + str(checksum)
+
+            h_size = eth_length + icmph_length
+            data_size = len(packet) - h_size
+
+            # Get data from packet
+            data = packet[h_size:]
+
+            print 'Data: ' + data
+            print '\n'
+        # UDP packets
+        elif protocol == 17:
+            u = iph_length + eth_length
+            udph_length = 8
+            udp_header = packet[u:u + 8]
+
+            # Unpack
+            udph = unpack('!HHHH', udp_header)
+            source_port = udph[0]
+            dest_port = udph[1]
+            length = udph[2]
+            checksum = udph[3]
+
+            print 'Source Port : ' + str(source_port) + '\n'
+            print 'Destination: ' + str(dest_port) + '\n'
+            print 'Length: ' + str(length) + '\n'
+            print 'Checksum: ' + str(checksum) + '\n'
+
+            h_size = eth_length + iph_length + udph_length
+            data_size = len(packet) - h_size
+            # Get data from packet
+            data = packet[h_size:]
+
+            print 'Data: ' + str(data)
+            print '\n'
+        else:
+            print 'Protocol other than TCP/UDP/ICMP'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    
---
+++ port_scanner.py
@@ -0,0 +1,147 @@
+# !/usr/bin/python
+# Author:Orandi Harris
+
+import socket
+import sys
+import datetime
+import time
+import argparse
+import os
+
+check = 0
+os.system('clear')
+line = "-" * 80
+desc = line +'''\nA Simple port scanner in python\n''' + line +"\n"
+
+parser = argparse.ArgumentParser(description = desc, formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument('host', metavar='H', help='Host name you want to scan')
+parser.add_argument('startport', metavar='P1', nargs='?', help='Start scanning from this port')
+parser.add_argument('endport', metavar='P2', nargs='?', help='Scan until this port')
+args = parser.parse_args()
+
+host = args.host
+ip = socket.gethostbyname(host)
+
+if(args.startport) and args.endport:
+    startport = int(args.startport)
+    endport = int(args.endport)
+else:
+    check = 1
+
+openPorts = []
+common_ports = {
+    '21': 'FTP',
+    '22': 'SSH',
+    '23': 'TELNET',
+    '25': 'SMTP',
+    '53': 'DNS',
+    '69': 'TFTP',
+    '80': 'HTTP',
+    '109': 'POP2',
+    '110': 'POP3',
+    '123': 'NTP',
+    '137': 'NETBIOS-NS',
+    '138': 'NETBIOS-DGM',
+    '139': 'NETBIOS-SSN',
+    '143': 'IMAP',
+    '156': 'SQL-SERVER',
+    '389': 'LDAP',
+    '443': 'HTTPS',
+    '546': 'DHCP-CLIENT',
+    '547': 'DHCP-SERVER',
+    '995': 'POP3-SSL',
+    '993': 'IMAP-SSL',
+    '2086': 'WHM/CPANEL',
+    '2087': 'WHM/CPANEL',
+    '2082': 'CPANEL',
+    '2083': 'CPANEL',
+    '3306': 'MYSQL',
+    '8443': 'PLESK',
+    '10000': 'VIRTUALMIN/WEBMIN'
+}
+
+start_Time = time.time()
+print "+" * 40
+print "\tSimple Port Scanner!"
+print "+" * 40
+
+if(check):
+    print "Scanning for most common ports"
+else:
+    print "Scanning %s from port %s - %s: " % (host, startport, endport)
+print "Scanning started at %s" % (time.strftime("%I:%M:%S %p"))
+
+def check_port(host, port, result = 1):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(0.5)
+        r = sock.connect_ex((host, port))
+        if r == 0:
+            result = r
+        sock.close()
+    except Exception, e:
+        pass
+    return result
+
+
+def get_service(port):
+    port = str(port)
+    if port in common_ports:
+        return common_ports[port]
+    else:
+        return 0
+
+try:
+    print "Scan in progres..."
+    print "Connecting to port: ",
+
+    if check:
+        for Port in sorted(common_ports):
+            sys.stdout.flush()
+            Port = int(Port)
+            print Port,
+            response = check_port(host, Port)
+            if response == 0:
+                openPorts.append(Port)
+                sys.stdout.write('\b' * len(str(Port)))
+    else:
+        for Port in range(startport, endport + 1):
+            sys.stdout.flush()
+            print Port,
+            response = check_port(host, Port)
+            if response == 0:
+                openPorts.append(Port)
+            if not Port == endport:
+                sys.stdout.write('\b' * len(str(Port)))
+    print "\nScanning completed at %s" % (time.strftime("%I:%M:%S %p"))
+    end_Time = time.time()
+    total_time = end_Time - start_Time
+    print "=" * 40
+    print "\tScan Report: %s" % (host)
+    print "=" * 40
+    if total_time <= 60:
+        total_time = str(round(total_time, 2))
+        print "Scan Took %s seconds" % (total_time)
+    else:
+        total_time = total_time / 60
+        print "Scan Took %s Minutes" % (total_time)
+
+    if openPorts:
+        print "Open Ports: "
+        print "\tPort Service Status"
+        for i in sorted(openPorts):
+            service = get_service(i)
+            if not service:
+                service = "Unknown service"
+            print "\t%s %s: Open" % (i, service)
+    else:
+        print "Sorry, No open ports found!"
+except KeyboardInterrupt:
+    print "You pressed Ctrl + C. Exiting"
+    sys.exit(1)
+
+
+
+
+
+
